a37533d9e38015859fabe91239cfd06fb78e356b
Macmillan1411 <macmillantapiwanashe1411>
2023-03-01 19:05:37 +0300

    created app projects and model Project and migrated it

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.TextField()
+    technology =models.CharField(max_length=50)
+    image = models.FilePathField(path="/img")
+
+    def __str__(self):
+        return self.title
+    
